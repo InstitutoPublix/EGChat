ffbdb7d783dc3b3fa31f5bd40c099d552f981f65
InstitutoPublix <consultores@institutopublix.com.br>
2025-04-01 19:17:21 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -213,7 +213,7 @@ div.stChatInput textarea::placeholder {
 )
 
 # Caminho para a logo do bot
-LOGO_BOT_PATH = "assets/icon_capr.jpg"
+LOGO_BOT_PATH = "assets/icon_tjce.jpg"
 
 # Verificar se o arquivo da logo existe
 if os.path.exists(LOGO_BOT_PATH):
